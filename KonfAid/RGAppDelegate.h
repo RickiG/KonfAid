bcc210659afcf53d6edf4771f723df589fd685a1
Ricki Gregersen <hi@input-squared.com>
2013-05-03 10:48:19 +0200

    Initial Commit

---
+++ KonfAid/RGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RGAppDelegate.h
+//  KonfAid
+//
+//  Created by Ricki Gregersen on 5/3/13.
+//  Copyright (c) 2013 Ricki Gregersen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KonfAid/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KonfAid
+//
+//  Created by Ricki Gregersen on 5/3/13.
+//  Copyright (c) 2013 Ricki Gregersen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RGAppDelegate class]));
+    }
+}
